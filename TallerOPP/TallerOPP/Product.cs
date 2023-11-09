429e29fb8b1fe846441bb09ba7e41384848499d6
omadurav <omadurav@outlook.com>
2023-11-09 19:52:13 +0100

    Implement interfaces and class Product

---
+++ TallerOPP/TallerOPP/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerOPP
+{
+    public abstract class Product : IPay
+    {
+        public string Description { get; set; }
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public float Tax { get; set; }
+        public abstract double GetValueToPay();
+
+        public override string ToString()
+        {
+            return $"";
+        }
+    }
+}
